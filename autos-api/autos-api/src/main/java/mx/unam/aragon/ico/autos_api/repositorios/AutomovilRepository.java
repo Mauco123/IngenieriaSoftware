c2442417dfc23970ab6840cd46931461fa239a78
Mauco123 <mauriciorodg17@gmail.com>
2025-04-21 17:52:19 -0600

    Delete autos-api/autos-api directory

--- autos-api/autos-api/src/main/java/mx/unam/aragon/ico/autos_api/repositorios/AutomovilRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.unam.aragon.ico.autos_api.repositorios;
-
-import mx.unam.aragon.ico.autos_api.modelos.Automovil;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutomovilRepository extends JpaRepository<Automovil, Long> {
-
-}
