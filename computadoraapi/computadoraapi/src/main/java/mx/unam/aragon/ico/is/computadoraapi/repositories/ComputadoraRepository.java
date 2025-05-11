b5a0b77cace3bee17fe129fc8fff7d90816aff39
Mauco123 <mauriciorodg17@gmail.com>
2025-05-11 16:06:35 -0600

    Delete computadoraapi/computadoraapi directory

--- computadoraapi/computadoraapi/src/main/java/mx/unam/aragon/ico/is/computadoraapi/repositories/ComputadoraRepository.java
+++
@@ -1,9 +0,0 @@
-package mx.unam.aragon.ico.is.computadoraapi.repositories;
-
-import mx.unam.aragon.ico.is.computadoraapi.entities.Computadora;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ComputadoraRepository extends CrudRepository<Computadora, Long> {
-    public Computadora findComputadoraByClave(Long clave);
-
-}
